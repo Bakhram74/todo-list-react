cbb01296ff4300e4894e1a7bf33fc9540faffb57
Bakhram <abdurakhimov93@list.ru>
2022-12-31 08:01:32 +0300

    login logout

---
+++ 1-full set project/src/app/App.tsx
@@ -0,0 +1,73 @@
+import React, {useEffect} from 'react'
+import './App.css'
+import {TodolistsList} from '../features/TodolistsList/TodolistsList'
+import {AppRootStateType, useAppDispatch, useAppSelector} from './store'
+import {initializeAppTC, RequestStatusType} from './app-reducer'
+import AppBar from '@mui/material/AppBar';
+import Toolbar from '@mui/material/Toolbar';
+import IconButton from '@mui/material/IconButton';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Container from '@mui/material/Container';
+import LinearProgress from '@mui/material/LinearProgress';
+import {Menu} from '@mui/icons-material';
+import {ErrorSnackbar} from '../components/ErrorSnackbar/ErrorSnackbar'
+import {Login} from "../features/Login/Login";
+import {Navigate, Route, Routes} from "react-router-dom";
+import {CircularProgress} from "@mui/material";
+import {useSelector} from "react-redux";
+import {logoutTC} from "../features/Login/auth-reducer";
+
+
+function App() {
+    const dispatch = useAppDispatch()
+    const status = useAppSelector<RequestStatusType>((state) => state.app.status)
+    const isInitialized = useSelector<AppRootStateType,boolean>(state => state.app.isInitialized)
+    const isLoggedIn = useSelector<AppRootStateType,boolean>(state => state.auth.isLoggedIn)
+
+    useEffect(()=>{
+        setTimeout(()=>{
+            dispatch(initializeAppTC())
+        },1000)
+
+    })
+
+    if (!isInitialized) {
+        return <div
+            style={{position: 'fixed', top: '30%', textAlign: 'center', width: '100%'}}>
+            <CircularProgress/>
+        </div>
+    }
+ const onclickHandler = ()=>{
+     dispatch(logoutTC())
+ }
+
+    return (
+        <div className="App">
+            <ErrorSnackbar/>
+            <AppBar position="static">
+                <Toolbar>
+                    <IconButton edge="start" color="inherit" aria-label="menu">
+                        <Menu/>
+                    </IconButton>
+                    <Typography variant="h6">
+                        News
+                    </Typography>
+                    {isLoggedIn && <Button onClick={onclickHandler} color="inherit">Logout</Button>}
+                </Toolbar>
+                {status === 'loading' && <LinearProgress/>}
+            </AppBar>
+            <Container fixed>
+                <Routes>
+                    <Route path={'/'} element={<TodolistsList/>}/>
+                    <Route path={'/login'} element={<Login/>}/>
+                    <Route path={'/404'} element={<h1 style={{textAlign:'center'}}>404: PAGE NOT FOUND</h1>}/>
+                    <Route path="*" element={<Navigate to="/404"/>}/>
+                </Routes>
+
+            </Container>
+        </div>
+    )
+}
+
+export default App
